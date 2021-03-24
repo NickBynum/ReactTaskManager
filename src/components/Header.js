bdb0af5ffa1f422b2c4de84bf6e9c7ae3b629df3
Nick Bynum <exander@live.com>
2021-03-24 13:39:43 -0600

    added task and add task

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,26 @@
-import PropTypes from 'prop-types'
-import Button from './Button'
+import PropTypes from "prop-types";
+import Button from "./Button";
 
-const Header = ({ title }) => {
-    const onClick = () => {
-        console.log('Click');
-    }
-    return (
-        <div>
-            <header className='header'>
-                <h1>{title}</h1>
-            </header>
-            <Button color='green' text='Add' 
-            onClick={onClick} />
-        </div>
-    )
-}
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <div>
+      <header className="header">
+        <h1>{title}</h1>
+      </header>
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? " Close" : "Add"}
+        onClick={onAdd}
+      />
+    </div>
+  );
+};
 
 Header.defaultProps = {
-    title: 'Task Tracker'
-}
+  title: "Task Tracker",
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired
-}
-export default Header
+  title: PropTypes.string.isRequired,
+};
+export default Header;
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,15 @@
-const tasks = []
-const Tasks = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import Task from './Task'
 
-export default Tasks
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <Task key={task.id} task={task}
+        onDelete={onDelete}
+        onToggle={onToggle}/>
+      ))}
+    </>
+  );
+};
+
+export default Tasks;
